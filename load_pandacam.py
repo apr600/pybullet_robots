708193e013d6b080d1f2dee086b67858eae624b2
Ahalya <ahalya.prabhakar@gmail.com>
2020-03-15 17:10:44 -0500

    made franka simulator with camera at end effector

---
+++ load_pandacam.py
@@ -0,0 +1,20 @@
+import pybullet as p
+import pybullet_data as pd
+import math
+import time
+import numpy as np
+import panda_cam
+
+p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP,1)
+p.setAdditionalSearchPath(pd.getDataPath())
+
+timeStep=1./60.
+p.setTimeStep(timeStep)
+p.setGravity(0,-9.8,0)
+ 
+panda = panda_cam.PandaCamSim(p,[0,0,0])
+while (1):
+	panda.step()
+	p.stepSimulation()
+	time.sleep(timeStep)
